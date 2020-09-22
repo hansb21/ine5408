fb4e0a7cb4438a46dd7e58f9b7afa5cff1ff3960
Hans Buss <hans.buss@mailfence.com>
2020-09-22 17:45:13 -0300

    Added Array Stack exercise

--- array_stack/array_stack.h
+++ array_stack/array_stack.h
@@ -1,4 +1,4 @@
-// Copyright [2019] <COLOQUE SEU NOME AQUI...>
+// Copyright [2020] <Hans Buss Heidemann.>
 #ifndef STRUCTURES_ARRAY_STACK_H
 #define STRUCTURES_ARRAY_STACK_H
 
@@ -56,7 +56,9 @@ structures::ArrayStack<T>::ArrayStack() {
 
 template<typename T>
 structures::ArrayStack<T>::ArrayStack(std::size_t max) {
-    // COLOQUE SEU CODIGO AQUI...
+    max_size_ = max;
+    contents = new T[max_size_];
+    top_ = -1;
 }
 
 template<typename T>
@@ -68,42 +70,50 @@ template<typename T>
 void structures::ArrayStack<T>::push(const T& data) {
     if (full()) {
         throw std::out_of_range("pilha cheia");
-    } else {
-        // COLOQUE SEU CODIGO AQUI...
-    }
 }
+       top_++;
+       contents[top_] = data;
+}
+
 
 template<typename T>
 T structures::ArrayStack<T>::pop() {
-    // COLOQUE SEU CODIGO AQUI...
+    if (empty()) {
+	throw std::out_of_range("pilha vazia");
+}
+	top_--;
+	return(contents[top_+1]);
 }
 
 template<typename T>
 T& structures::ArrayStack<T>::top() {
-    // COLOQUE SEU CODIGO AQUI...
+    if (empty()) {
+	throw std::out_of_range("pilha vazia");
+}
+	return(contents[top_]);
 }
 
 template<typename T>
 void structures::ArrayStack<T>::clear() {
-    // COLOQUE SEU CODIGO AQUI...
+    top_ = -1;
 }
 
 template<typename T>
 std::size_t structures::ArrayStack<T>::size() {
-    // COLOQUE SEU CODIGO AQUI...
+    return top_ +1;
 }
 
 template<typename T>
 std::size_t structures::ArrayStack<T>::max_size() {
-    // COLOQUE SEU CODIGO AQUI...
+    return(max_size_);
 }
 
 template<typename T>
 bool structures::ArrayStack<T>::empty() {
-    // COLOQUE SEU CODIGO AQUI...
-}
+    return (top_ == -1);
+    }
 
 template<typename T>
 bool structures::ArrayStack<T>::full() {
-    // COLOQUE SEU CODIGO AQUI...
+    return (top_ == max_size_ -1);
 }
